0663ba6acc664c1d50c9197a04945bff2ebc3ad7
Jennifer Ross <ross.m.jennifer@gmail.com>
2019-08-06 16:51:28 -0400

    hover effect on portfolio items working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
-$('.card-title').hide();
-$('.card-subtitle').hide(); 
-$('.btn').hide(); 
+// $('.card-title').hide();
+// $('.card-subtitle').hide(); 
+// $('.btn').hide(); 
+
+// // function show() {
+// //     $('.card-title').show();
+// //     $('.card-subtitle').show(); 
+// //     $('.btn').show(); 
+// // }
+
+// $('.hangman-p-item').mouseenter(function(){
+//     $('.hangman').show();
+//     $('.card-img-overlay').css({ 'opacity': '0.3' });
+// });
+
+// $('.hangman-p-item').mouseleave(function(){
+//     $('.hangman').hide();
+// });
